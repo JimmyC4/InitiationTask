6af5e3a914c86c5294e2a5775572a95b5a95dc66
JimmyC4 <jameskang99@outlook.com>
2022-12-23 22:23:59 +0800

    updating filter dropdown menu

---
+++ src/components/Filter.js
@@ -0,0 +1,21 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { TaskListContext } from '../contexts/TaskListContext'
+import "../AddProject.css";
+
+const Filter = () => {
+  const [openFilter, setOpenFilter ] = useState(false);
+  
+
+  return (
+    <div className='relative-container'>
+      <div className='absolute-container absolute-container2'>
+        <ul>
+          <span><input type="checkbox" className="checkbox"/>User</span>
+          <span><input type="checkbox" className="checkbox"/>Is Completed</span>   
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -3,9 +3,11 @@ import { TaskListContext } from '../contexts/TaskListContext'
 import plus from '../images/icons/plus.png';
 import search from '../images/icons/search.png';
 import AddProject from './AddProject';
+import Filter from './Filter';
 
 const TaskForm = () => {
   const [openAddProject, setOpenAddProject ] = useState(false);
+  const [openFilter, setOpenFilter ] = useState(false);
 
   const { addTask, clearList, editTask, editItem } = useContext(TaskListContext)
   const [title, setTitle] = useState('')
@@ -47,15 +49,15 @@ const TaskForm = () => {
           Add Project
         </button>
 
-        {
-          openAddProject && <AddProject/>
-        }
-        
-
-        <button className="button filter-btn" onClick="">
+        <button className="button filter-btn" onClick={()=> setOpenFilter((prev) => !prev)}>
           Add Filter
         </button>
+        
+          {openAddProject && <AddProject/>}
+          {openFilter && <Filter/>}
+
     </div>
+    
   )
 }
 
